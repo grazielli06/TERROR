199cfe26fea07ee9445efad0ee74287476326e9c
grazielli <113601224+grazielli06@users.noreply.github.com>
2024-08-22 20:11:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ const perguntas = [
     {
         enunciado: "Em uma noite com tempestades. Você e seus amigos decidem investigar uma casa abandonada no final da rua, famosa por suas lendas assustadoras. O vento uivava, e relâmpagos iluminavam o céu enquanto vocês se aproximavam da porta range",
         alternativas: [
-            "Entrar pela frente.",
-            "Tentar uma janela."
-        ]
+             "entrar na casa"
+            "verificar janela"
+        
     },
     {
         enunciado: "Ao entrar, você se depara com o cheiro  forte de podridão. A luz do relâmpago revela uma mesa com objetos estranhos, e você sente uma presença atrás de você.",
         alternativas: [
-            "Virar-se para ver a presença.",
+            "Ao entrar, você se depara com o cheiro  forte de podridão. A luz do relâmpago revela uma mesa com objetos estranhos, e você sente uma presença atrás de você",
             "Ignorar e explorar a sala. ",
         ]
     },
     {
-        pergunta: "A luz do relamgago revel uma figura esquelética, com olhos vazios e uma expressão de desespero congelada em seu rosto.  A presença parece imóvel,como se estivesse esperando por algo.",
+        pergunta: "congelada em seu rosto.  A presença parece imóvel,como se estivesse esperando por algo.A luz do relamgago revel uma figura esquelética, com olhos vazios e uma expressão de desespero ",
         alternativas: [
             "Tentae se comunicar com a figura.",
             "Ignorar a figura e explorar mais a sala."
@@ -30,7 +30,8 @@ const perguntas = [
         enunciado: "Á medida que você se afasta, o cheiro de podridão se torna mais forte, quase insuportável. Você nota um armário encostado na parede, com portas entreabertas. Dentro, há uma pilha de livros antigos e uma pequena caixa de madeira. ",
         alternativas: [
             "Examinar os livros no armário.",
-            "Abrir a caixa de madeira."
+            "            "Examinar os livros no armário.",
+"
         ]
     },
     {
@@ -38,24 +39,45 @@ const perguntas = [
         alternativas: [
            "Continuar lendo.",
             "Procurar mais pistas na sala."
+        
+        
         ]
     },
 ];
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
-    }
+    }Em uma noite com tempestades. Você e seus amigos decidem investigar uma casa abandonada no final da rua, famosa por suas lendas assustadoras. O vento uivava, e relâmpagos iluminavam o céu enquanto vocês se aproximavam da porta range",
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
